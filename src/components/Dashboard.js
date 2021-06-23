883dcab3fdeafb5e3b2526d38c2a704751997c13
Karla Nieves <karlaxnieves@gmail.com>
2021-06-23 11:54:59 -0700

    added new components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTech} from "../store";
+import Tech from "./Tech";
+
+function Dashboard(props) {
+    console.log(props.currentUser.auth_code)
+
+  useEffect(() => {
+    props.getTech();
+  }, []);
+
+  return (
+    <div>
+      <h2>Available Rentals</h2>
+      { props.currentUser.auth_code && <Link to="/techform">Add Equipment</Link>}
+      <div>
+        {props.loading && <h2>Loading Equipment...</h2>}
+
+        {props.tech.map &&
+          props.tech.map((item) => {
+            return <Tech key={item.tech_id} item={item} />;
+          })}
+
+        {props.errors && <h2>{props.errors}</h2>}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    tech: state.tech,
+    loading: state.loading,
+    errors: state.errors,
+    currentUser: state.currentUser
+  };
+};
+
+export default connect(mapStateToProps, { getTech })(Dashboard);
---
+++ src/components/EditTech.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { editTech } from "../store/actions";
+import { connect } from "react-redux";
+
+function EditTech(props) {
+  const { id } = useParams();
+  const history = useHistory();
+  const [editTech, setEditTech] = useState({
+    tech_image: "",
+    tech_type: "",
+    day_duration: "",
+    start_date: "",
+  });
+
+  const onChange = (e) => {
+    setEditTech({
+      ...editClass,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    props.editTech(id, editTech);
+    history.push("/tech");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+       <label>
+          Type:
+          <input
+            type="text"
+            value={values.tech_type}
+            onChange={onChange}
+            name="tech_type"
+          />
+        </label>
+        <label>
+          Image:
+          <input
+            type="text"
+            value={values.tech_image}
+            onChange={onChange}
+            name="tech_image"
+          />
+        </label>
+        <label>
+          Duration:
+          <input
+            type="text"
+            value={values.day_duration}
+            onChange={onChange}
+            name="day_duration"
+          />
+        </label>
+        <label>
+          Start Date:
+          <input
+            type="text"
+            value={values.start_date}
+            onChange={onChange}
+            name="start_date"
+          />
+        </label>
+        <button>Edit Equipment</button>
+      </form>
+  );
+}
+
+export default connect(null, {editTech})(EditTech);
