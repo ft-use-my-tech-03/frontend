7232d231030e075371a39053f61e1a01e2cae7b4
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-06-24 10:42:55 -0400

    schema didnt have phone number defined broke sign up form - fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
             <p>
             Much like AirBnB, but for high end electronics. Are you tired of paying ridiculous fees for camera and other equipment rentals? Bypass the middleman and rent from a real person!
             </p>
+            <img src="https://ma-hub.imgix.net/wp-images/2019/07/22174711/cameras-renting-vs-owning.jpg?w=800&h=400&auto=format" />
         </div>
     )
 }
