6d7be71ecc15beb1f76aaf2ef1cc0d37bc30ea4c
Praxis Technologies <ptanalytics@gmail.com>
2021-06-25 12:22:37 -0400

    added link to product details page from individual products

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import "./card.css"
 import { Link } from 'react-router-dom'
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+  import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Card = (item) => {
     return (
         <div className="card-section">
             <div className="image-sec">
-             <Link to={`/products/${item.item.id}`}>
+             <Link to={`/details/${item.item.item_id}`}>
                 <img src={item.item.image_url} alt="..." style={{width: "200px", height: "200px"}}/>
                 </Link>
             </div>
             <div className="body">
-            <Link to={`/products/${item.item.id}`}> <h3 style={{color: "white"}}>{item.item.item_name}</h3> </Link>
+            <Link to={`/details/${item.item.item_id}`}> <h3 style={{color: "white"}}>{item.item.item_name}</h3> </Link>
                 <p>{item.item.item_category}</p>
                 <p>{item.item.item_description}</p>
                 <div>
